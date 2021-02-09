41f49ccc12a6397a74f3f4141ed18c977d38836f
Tobias van Beek <t.vanbeek@tjvb.nl>
2021-02-09 14:11:59 +0100

    Fix Psalm level 6 errors

--- src/MailEventSubscriber.php
+++ src/MailEventSubscriber.php
@@ -14,7 +14,7 @@ class MailEventSubscriber
     /**
      * Register the listeners for the subscriber.
      *
-     * @param  Illuminate\Events\Dispatcher  $events
+     * @param  \Illuminate\Events\Dispatcher  $events
      *
      * @return void
      */
