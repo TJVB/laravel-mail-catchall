df05a9c0f1decca8e5060bc331373846982994d8
Blue Dragon developers <developers@bluedragon.nl>
2026-03-26 08:53:16 +0000

    Update code-style

--- tests/MailCatcherTest.php
+++ tests/MailCatcherTest.php
@@ -9,9 +9,9 @@ use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\View\Factory as ViewFactory;
 use Illuminate\Mail\Events\MessageSending;
 use Mockery;
+use Mockery\MockInterface;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
-use Mockery\MockInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
@@ -152,7 +152,7 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.receiver' => $receiver]);
 
         $message = new Email();
-        $message->text($faker->text);
+        $message->text($faker->text());
         $event = new MessageSending($message);
 
         // run
@@ -181,7 +181,7 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.receiver' => $receiver]);
 
         $message = new Email();
-        $message->cc($faker->email);
+        $message->cc($faker->email());
 
         $event = new MessageSending($message);
 
@@ -213,8 +213,8 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.receiver' => $receiver]);
 
         $message = new Email();
-        $message->text($faker->text);
-        $message->addBcc($faker->email);
+        $message->text($faker->text());
+        $message->addBcc($faker->email());
 
         $event = new MessageSending($message);
 
@@ -249,7 +249,7 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.add_receivers_to_text' => true]);
 
         $message = new Email();
-        $message->text($faker->text);
+        $message->text($faker->text());
         if (is_array($originalReceiver)) {
             $message->to(...$originalReceiver);
         } else {
@@ -295,7 +295,7 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.add_receivers_to_html' => true]);
 
         $message = new Email();
-        $message->html($faker->text);
+        $message->html($faker->text());
         if (is_array($originalReceiver)) {
             $message->to(...$originalReceiver);
         } else {
@@ -340,7 +340,7 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.receiver' => $receiver]);
 
         $message = new Email();
-        $message->html($faker->text);
+        $message->html($faker->text());
         if (is_array($originalReceiver)) {
             $message->to(...$originalReceiver);
         } else {
@@ -386,7 +386,7 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.add_receivers_to_text' => true]);
 
         $message = new Email();
-        $message->text($faker->text);
+        $message->text($faker->text());
         if (is_array($originalReceiver)) {
             $message->cc(...$originalReceiver);
         } else {
@@ -433,7 +433,7 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.add_receivers_to_html' => true]);
 
         $message = new Email();
-        $message->html($faker->text);
+        $message->html($faker->text());
         if (is_array($originalReceiver)) {
             $message->cc(...$originalReceiver);
         } else {
@@ -480,7 +480,7 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.add_receivers_to_text' => true]);
 
         $message = new Email();
-        $message->text($faker->text);
+        $message->text($faker->text());
         if (is_array($originalReceiver)) {
             $message->bcc(...$originalReceiver);
         } else {
@@ -527,7 +527,7 @@ final class MailCatcherTest extends TestCase
         config(['mailcatchall.add_receivers_to_html' => true]);
 
         $message = new Email();
-        $message->html($faker->text);
+        $message->html($faker->text());
         if (is_array($originalReceiver)) {
             $message->bcc(...$originalReceiver);
         } else {
@@ -565,13 +565,13 @@ final class MailCatcherTest extends TestCase
         $faker = Factory::create();
         return [
             'email' => [
-                $faker->email
+                $faker->email()
             ],
             'email array' => [
-                [$faker->email]
+                [$faker->email()]
             ],
             'address class' => [
-                new Address($faker->email),
+                new Address($faker->email()),
             ],
         ];
     }
